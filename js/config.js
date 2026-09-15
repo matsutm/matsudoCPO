bb7a8d2f9b8203ab5fd7c9cc45b317d28a42407a
matsutm <naritatomoyuki@gmail.com>
2026-09-15 15:49:12 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 //メール送信機能は本番環境のみで実現する。またはテストで切り替える
 const EMAIL_NOTIFY_ENABLED = false; //本番では必ず true
-const DEV_AUTO_LOGIN = true; // 本番では必ず false
+const DEV_AUTO_LOGIN = false; // 本番では必ず false
 
 const DEV_TEST_USER_ID = '320e7ac5-a3e7-4241-94d8-619ad4b1e9f3';
 
