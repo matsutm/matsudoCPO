dbe44aeb77770a7e52e23011158ad13bf9676d6a
matsutm <naritatomoyuki@gmail.com>
2026-09-16 08:28:52 +0900

    Delete js/utils/action-util.js

--- js/utils/action-util.js
+++
@@ -1,9 +0,0 @@
-// js/utils/action-utils.js
-
-export async function confirmAndRun(message, action, successMessage) {
-  if (!confirm(message)) return;
-
-  await action();
-
-  alert(successMessage);
-}
