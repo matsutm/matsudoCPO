8ea99f7a02756e033e8aa979b67e71c31fe6fd7d
matsutm <naritatomoyuki@gmail.com>
2026-09-18 11:22:28 +0900

    Add files via upload

--- js/components/calendar-modal.js
+++ js/components/calendar-modal.js
@@ -6,17 +6,8 @@ import { createSchedule, updateSchedule, deleteSchedule } from '../services/cale
 import { confirmAndRun } from '../utils/action-utils.js';
 
 export function openCalendarModal({ mode, event, onSaved }) {
-  const p = {
-    ...(event?.extendedProps || {}),
-    ...(event || {})
-  };
+  const p = event?.extendedProps || {};
 
-  // raw_date や notes のプロパティ名の表記揺れを補正して統一
-  p.raw_date = p.raw_date || p.date || (event?.startStr ? event.startStr.split('T')[0] : '');
-  p.notes = p.notes || p.program_notes || '';
-  p.location = p.location || '';
-  p.instructor = p.instructor || '';
-  
   // ★ FullCalendar の ID を安全に取得
   const scheduleId = event?.id || p.id;
 
