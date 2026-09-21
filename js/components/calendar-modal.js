6da932a21755fdedb53183ccffe0eb7a084761ff
matsutm <naritatomoyuki@gmail.com>
2026-09-21 22:02:33 +0900

    Add files via upload

--- js/components/calendar-modal.js
+++ js/components/calendar-modal.js
@@ -67,7 +67,8 @@ function renderViewContent(data = {}) {
     <div class="form-group">
       <label for="location">場所</label>
       <div class="detail-text">📍 ${data.location || '未定'}</div>
-      <!-- ${renderMapButton(data.location)} -->
+      <!--  -->
+      
     </div>
 
     <div class="form-group">
